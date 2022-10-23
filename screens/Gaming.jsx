43cc276a75c1a1b1c266d51cf6982a6b9b161bdf
beqakhelidze <khelidzeb@gmail.com>
2022-10-23 19:43:41 +0400

    Bus version

--- screens/Gaming.jsx
+++ screens/Gaming.jsx
@@ -1,32 +1,53 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import GlobalStyles from "../styles";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import io from 'socket.io-client';
 
-const socket = io('http://192.168.100.99:3000/rooms?key=bcbb7262-5154-4ca2-b5cb-4ea1d9379a6a&username=givi',
-    {
-        transports: ["websocket", "polling"],
-    },
-);
+let socket;
 
+const setSocket = async(value) => {
+    socket = io('http://192.168.100.99:3000/rooms?key='+value+'&username=givi123',
+        {
+            transports: ["websocket", "polling"],
+        },
+    );
+}
 
 const Gaming = () => {
 
+    const [gameInfo, setGameInfo] = useState({});
+
     const setEvents = async () => {
         const value = await AsyncStorage.getItem('RoomKey');
+        await setSocket(value);
+        socket.on("game.data.received", (data) =>{
+            console.log(data);
+            setGameInfo(data);
+        })
     }
 
     useEffect(() => {
         setEvents();
     }, []);
 
-    console.log(socket);
 
     return (
-        <View>
-            <Text>Gaming</Text>
+        <View style={GlobalStyles.container}>
+            <View style={styles.gameInfo}>
+                <Text>Max Players:{gameInfo.maxPlayers}</Text>
+                <Text>Duration:{gameInfo.gameDurationSeconds}</Text>
+                <Text>Duration:{gameInfo.gameDurationSeconds}</Text>
+                <Text>Duration:{gameInfo.hintsEnabled}</Text>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    gameInfo:{
+
+    }
+})
 
 export default Gaming;
