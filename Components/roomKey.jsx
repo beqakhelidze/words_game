cc1f876cc043720147643583291f69168c3b0de8
beqakhelidze <khelidzeb@gmail.com>
2022-10-28 16:32:16 +0400

    Clipboard is Added.

--- Components/roomKey.jsx
+++ Components/roomKey.jsx
@@ -1,21 +1,33 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect, useState } from "react";
-import { Alert, Modal, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+    Alert,
+    Modal,
+    StyleSheet,
+    Text, View,
+    TouchableOpacity,
+} from "react-native";
+import * as Clipboard from 'expo-clipboard';
 import GlobalStyles from "../styles";
 
 
 const RoomKey = ({ modalVisible, setModalVisible, }) => {
 
-    const [key,setKey] = useState("");
+    const [key, setKey] = useState("");
 
-   const onLoad = async() =>{
-     const storage = await AsyncStorage.getItem("RoomKey")
-     setKey(storage);
-   }
+    const onLoad = async () => {
+        const storage = await AsyncStorage.getItem("RoomKey")
+        setKey(storage);
+    }
 
-    useEffect(() =>{
-       onLoad();
-    },[])
+    const copyToClipboard = () => {
+        console.log(key);
+        Clipboard.setStringAsync(key);
+    };
+
+    useEffect(() => {
+        onLoad();
+    }, [])
 
     return (
         <View style={GlobalStyles.centeredView}>
@@ -29,10 +41,13 @@ const RoomKey = ({ modalVisible, setModalVisible, }) => {
             >
                 <View style={GlobalStyles.centeredView}>
                     <View style={GlobalStyles.modalView}>
+
                         <Text style={GlobalStyles.Title}>
                             Room Key
                         </Text>
-                        <Text style={styles.key}>{key}</Text>
+                        <TouchableOpacity onPress={copyToClipboard}>
+                            <Text style={styles.key}>{key}</Text>
+                        </TouchableOpacity>
                         <TouchableOpacity
                             style={GlobalStyles.button}
                             onPress={() => setModalVisible(!modalVisible)}
@@ -47,9 +62,9 @@ const RoomKey = ({ modalVisible, setModalVisible, }) => {
 }
 
 const styles = StyleSheet.create({
-    key:{
-        fontSize:16,
-        marginBottom:13,
+    key: {
+        fontSize: 16,
+        marginBottom: 13,
     }
 });
 
